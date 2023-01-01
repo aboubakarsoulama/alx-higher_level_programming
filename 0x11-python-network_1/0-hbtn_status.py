7fecdd7e94de1712c28dfc458732963f56650183
aboubakarsoulama <totypopito306@gmail.com>
2023-01-01 14:12:14 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,16 @@
-dy response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {#!/usr/bin/python3
-
+#!/usr/bin/python3
 """A script that
-- fetches the url: https://alx-intranet.hbtn.io/status.
-- uses urllib package
+- fetches https://intranet.hbtn.io/status.
+- uses urlib package
 """
 
 
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
-        print("Bo}".format(content.decode('utf-8')))
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
